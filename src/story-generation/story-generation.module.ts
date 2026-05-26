bee77032b2c3d7d401e3996d2e05d14b11f38a79
Sencoool <s6506021610037@email.kmutnb.ac.th>
2026-05-26 23:14:01 +0700

    feat: update CI to Node 24 and add E2E testing step; fix story generation module imports

--- src/story-generation/story-generation.module.ts
+++ src/story-generation/story-generation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RabbitMqModule } from '../messaging/rabbitmq.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { StoryGenerationController } from './story-generation.controller';
 import { StoryGenerationService } from './story-generation.service';
 
 @Module({
+  imports: [PrismaModule, RabbitMqModule],
   controllers: [StoryGenerationController],
   providers: [StoryGenerationService],
 })
